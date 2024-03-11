6af34df5722969d901a2dced7b10d6ea93b65f23
gustavoc_04 <gustavodecastrocastilhocosta@outlook.com>
2024-03-11 12:12:19 -0300

    modal funcionando

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { NavBar } from '../components/NavBar/NavBar'
 
 export const About = () => {
     return(
         <>
             <p>PÁGINA SOBRES</p>
+            <NavBar/>
         </>
     )
 }
--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { NavBar } from '../components/NavBar/NavBar'
 
 export const FeedBack = () => {
     return(
         <>
             <p>PÁGINA FEEDBACK</p>
+            <NavBar/>
         </>
     )
 }
